4151d169586bc688a7e2c714baddcb95257542a8
Devesh <deveshrai@github.com>
2020-03-19 15:38:04 +0530

    V0.1 Code

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,72 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->statusBar()->showMessage("Not Connected");
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::onConnected()
+{
+    this->statusBar()->showMessage("Connected");
+}
+
+void MainWindow::onDisconnect()
+{
+    this->statusBar()->showMessage("Disconnected");
+}
+
+void MainWindow::onTextRecieved(QString message)
+{
+    ui->rXplainTextEdit->appendPlainText(message);
+}
+
+void MainWindow::onBinaryRecieved(QByteArray message)
+{
+    qDebug()<<"Recieved: "<<message.size();
+}
+
+void MainWindow::on_connectButton_clicked()
+{
+    delete(ws);
+    this->ws = new QWebSocket();
+    connect(ws, &QWebSocket::connected, this, &MainWindow::onConnected);
+    connect(ws, &QWebSocket::disconnected, this, &MainWindow::onDisconnect);
+    connect(ws, &QWebSocket::textMessageReceived, this, &MainWindow::onTextRecieved);
+    connect(ws, &QWebSocket::binaryMessageReceived, this, &MainWindow::onBinaryRecieved);
+
+    ws->open(QUrl("ws://"+ui->addressLineEdit->text()));
+}
+
+void MainWindow::on_textSendButton_clicked()
+{
+    ws->sendTextMessage(ui->tXplainTextEdit->toPlainText());
+}
+
+void MainWindow::on_chooseFileButton_clicked()
+{
+
+    QFileDialog fD(this);
+//    fD.exec();
+    QFile file;
+    file.setFileName(fD.getOpenFileName());
+    file.open(QIODevice::ReadOnly);
+    data.clear();
+    data=file.readAll();
+//    data.append(data);
+//    data.append(data);
+//    data.append(data);
+
+    qDebug()<<data.size();
+}
+
+void MainWindow::on_fileSendButton_clicked()
+{
+    if(data.size()>1)
+        ws->sendBinaryMessage(data);
+
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QWebSocket>
+#include <QFileDialog>
+#include <QFile>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +18,24 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void onConnected();
+    void onDisconnect();
+    void onTextRecieved(QString message);
+    void onBinaryRecieved(QByteArray message);
+
+    void on_connectButton_clicked();
+
+    void on_textSendButton_clicked();
+
+    void on_chooseFileButton_clicked();
+
+    void on_fileSendButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QWebSocket *ws;
+    QByteArray data;
 };
 
 #endif // MAINWINDOW_H
